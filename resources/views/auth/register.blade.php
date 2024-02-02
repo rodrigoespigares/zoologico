cc197f1abcc329025ce1a8a9bb76bda30b3571b8
rodrigoespigares <rodrigoespigaresfernandez@gmail.com>
2024-02-02 09:01:32 +0100

    cambios en usuario y en el registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,9 +4,16 @@
 
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
+            <x-input-label for="name" :value="__('Nombre')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        </div>
+
+        <!-- Apellidos -->
+        <div class="mt-4">
+            <x-input-label for="subname" :value="__('Apellidos')" />
+            <x-text-input id="subname" class="block mt-1 w-full" type="text" name="subname" :value="old('subname')" required autofocus autocomplete="subname" />
+            <x-input-error :messages="$errors->get('subname')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
@@ -18,7 +25,7 @@
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Contraseña')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -30,7 +37,7 @@
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-input-label for="password_confirmation" :value="__('Confirma Contraseña')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"

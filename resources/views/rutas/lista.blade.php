7418bdba24ced6eca994ff520059635fbff9c8e4
Jose Miguel <josemiguel41104@gmail.com>
2024-02-06 11:01:45 +0100

    Cambio a la vista de las rutas para que sea igual a los animales

--- resources/views/rutas/lista.blade.php
+++ resources/views/rutas/lista.blade.php
@@ -2,6 +2,32 @@
 <x-app-layout>
     <x-slot name="slot">
 
+    <div class="container__all_cards">
+        @foreach ($resultados as $resultado)
+            @if ($resultado->visitable == 1)
+                @php
+                    if($resultado->foto == "sin_foto.png"){
+                        $foto = url('img/'.$resultado->foto);
+                    }else{
+                        $foto = url('img/subidas/rutas'.$resultado->foto);
+                    }
+
+                @endphp
+                <a class="flip-container" href="/rutas/ver/{{$resultado->id}}">
+                    <div class="card">
+                        <div class="frente">
+                            <div class="contenedor__img"><img src="{{$foto}}" alt=""></div>
+                        </div>
+                        <div class="dorso">
+                            <h3 style="height: 10%;  margin-top: 30px">{{$resultado->nombre}}</h3>
+                            <p>{{$resultado->descripcion}}</p>
+                        </div>
+                    </div>
+                </a>
+            @endif
+        @endforeach
+    </div>
+        <!--
     <table class="table">
         <tr>
             <th>Nombre</th>
@@ -10,14 +36,7 @@
         </tr>
         @foreach ($resultados as $resultado)
 
-            @php
-            if($resultado->foto == "sin_foto.png"){
-                $foto = url('img/'.$resultado->foto);
-            }else{
-                $foto = url('img/subidas/rutas'.$resultado->foto);
-            }
 
-            @endphp
             @if ($resultado->visitable == 1)
                 <tr>
                     <td>{{$resultado->nombre}}</td>
@@ -33,5 +52,6 @@
 
 
     </table>
+    -->
 </x-slot>
 </x-app-layout>

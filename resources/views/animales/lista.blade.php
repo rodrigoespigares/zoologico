d4cec984683afbc1b6ce9b26f9e125496ea9c9ef
Jose Miguel <josemiguel41104@gmail.com>
2024-02-06 10:52:51 +0100

    Css cambio card pequeño y en las vista añade el ver al clikar en una card

--- resources/views/animales/lista.blade.php
+++ resources/views/animales/lista.blade.php
@@ -4,10 +4,10 @@
     <div class="container__all_cards">
         @foreach ($resultados as $resultado)
             @if ($resultado->visitable == 1 && in_array($resultado->ruta_id,$rutas->toArray()))
-                <div class="flip-container">
+                <a class="flip-container" href="/animales/ver/{{$resultado->id}}">
                     <div class="card">
                         <div class="frente">
-                            <div class="hola"><img src="{{url('img/subidas/'.$resultado->foto)}}" alt=""></div>
+                            <div class="contenedor__img"><img src="{{url('img/subidas/animales/'.$resultado->foto)}}" alt=""></div>
                         </div>
                         <div class="dorso">
                             <h3>{{$resultado->nombre}}</h3>
@@ -15,7 +15,7 @@
                             <p>{{$resultado->descripcion}}</p>
                         </div>
                     </div>
-                </div>
+                </a>
             @endif
         @endforeach
     </div>
